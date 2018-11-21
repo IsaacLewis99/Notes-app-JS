9f3c27fe900eaeedc232287164f2d6ede4aa4645
Isaac Lewis <isaaclewis@isaacs-mbp.home>
2018-11-21 13:31:49 +0000

    First note test success

--- src/note-model.js
+++ src/note-model.js
@@ -1,7 +1,14 @@
 (function(exports) {
-   function NoteModel() {
-     this.hello = "Hello World!";
-   };
+  function Note()  {
+    this.notes = []
+  };
+  Note.prototype.newNote = function(content) {
+   this.notes.push(content);
+ };
+ Note.prototype.getNote = function(number) {
+   console.log(this.notes[number-1]);
+   return this.notes[number-1];
+ }
+  exports.Note = Note;
 
-   exports.NoteModel = NoteModel;
- })(this);
+})(this);
