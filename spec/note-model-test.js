720ef58a65952406e8db4892e8ad12392afa4c29
Isaac Lewis <isaaclewis@isaacs-mbp.home>
2018-11-21 10:33:27 +0000

    first test + updated index

---
+++ spec/note-model-test.js
@@ -0,0 +1,14 @@
+(function(exports) {
+
+   function testHelloWorld() {
+     var helloWorld = new NoteModel();
+
+     if (helloWorld.hello !== "Hello World!") {
+       throw new Error("Not Hello world");
+     } else {
+       console.log(".");
+     }
+   };
+
+   testHelloWorld();
+ })(this);
